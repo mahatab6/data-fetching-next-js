b30ac9020b9f14f65e658a8e154ba464d1fc8413
mahatab6 <mahatabcall56@gmail.com>
2025-08-01 20:59:08 +0600

    singlepost details find dne

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-export default  function PostDetails({params}) {
-    const p =  params.id;
-    console.log(p) 
+export const singlepostId = async (id)=>{
+    
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const data = await res.json();
+    return data;
+}
+
+export default async function PostDetails({params}) {
+
+    const id =  params.id;
+    const post = await singlepostId(id);
+    console.log(post)
 
   return (
-    <div>PostDetails- {p}</div>
+    <div className='text-center'>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+        
+    </div>
   )
 }
