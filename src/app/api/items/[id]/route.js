ad2b82800866ffe712cf03a598163d8a6f6b790d
mahatab6 <mahatabcall56@gmail.com>
2025-08-04 16:14:45 +0600

    delete, patch, single data find

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,21 +1,34 @@
+import { dbConnect } from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
 
-export async function GET(req, params) {
+export async function GET(req, {params}) {
   
    const p = await params;
- 
-  return Response.json({ params: p })
+   const {db} = await dbConnect();
+   const singleData = await db.collection("user").findOne({_id: new ObjectId(p)});
+   return Response.json(singleData);
+
 }
 
-export async function DELETE(req, params) {
+export async function DELETE(req, {params}) {
   
    const p = await params;
- 
-  return Response.json({ params: p })
+   const {db} = await dbConnect();
+   const deleteData = await db.collection("user").deleteOne({_id: new ObjectId(p)});
+   return Response.json(deleteData);
 }
 
-export async function PATCH(req, params) {
+export async function PATCH(req, {params}) {
   
-   const p = await params;
- 
-  return Response.json({ params: p })
+   const { id } =  params;
+   const postDate = await req.json();
+   
+   const {db} = await dbConnect();
+
+   const filter = {_id: new ObjectId(id)};
+   const update = { $set: postDate };
+
+  
+   const dataUpdata = await db.collection("user").updateOne(filter, update);
+   return Response.json(dataUpdata);
 }
