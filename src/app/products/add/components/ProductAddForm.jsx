5ba2800c5882b85f4eaecbe4b7d1f604dc50904f
mahatab6 <mahatabcall56@gmail.com>
2025-08-07 15:37:02 +0600

    data sent to database

--- src/app/products/add/components/ProductAddForm.jsx
+++ src/app/products/add/components/ProductAddForm.jsx
@@ -1,22 +1,31 @@
 "use client"
 export default function ProductAddForm() {
-    const handleForm = (e) => {
-        e.PreventDefault();
+    const handleForm = async (e) => {
+        e.preventDefault();
+        const formelement = e.target;
+        const form = formelement.name.value;
+        const formData = {form};
+        const res = await fetch("http://localhost:3000/api/items",{
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const result = await res.json();
+        formelement.reset();
+        alert("Post sent");
 
     }
 
   return (
-    <div>
-        <form onSubmit={handleForm}>
+    <div className=" justify-items-center text-center">
+        <form className=" space-y-2" onSubmit={handleForm}>
             <div>
-                <label htmlFor="">Product Name</label>
-                <input type="text" />
+                <label className="block" htmlFor="">Product Name</label>
+                <input className=" dark:bg-white border-2 border-black text-xl dark:text-black" type="text" name="name"/>
             </div>
-            <div>
-                <label htmlFor="">Email</label>
-                <input type="email" />
-            </div>
-            <button type='submit'>Submit</button>
+            <button className="bg-purple-700 p-2 rounded-2xl" type='submit'>Submit</button>
         </form>
     </div>
   )
--- src/app/products/add/page.jsx
+++ src/app/products/add/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import ProductAddForm from './components/ProductAddForm'
 
 export default function ProductAddPage() {
   return (
     <div>
-        <h1>Add New Product</h1>
-        
+        <h1 className='text-center text-4xl my-10'>Add New Product</h1>
+        <div>
+            <ProductAddForm/>
+        </div>
     </div>
   )
 }
